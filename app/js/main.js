8ca9c467d02132a326040bee14db71527cbd5320
thomasweld <thomas.weld@gmail.com>
2016-04-12 22:58:37 -0400

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+
+// CLOCK
+
+// set initial time on clock for page load
+document.querySelector('#clock').innerHTML = (new Date()).toLocaleTimeString();
+
+// setInterval says how often to run timeGoesOn function which
+//advances the clock every 1000ms or 1 second
+setInterval( timeGoesOn, 1000);
+
+// function is run every 1000ms and assigns
+function timeGoesOn(){
+  document.querySelector('#clock').innerHTML = (new Date()).toLocaleTimeString();
+};
+
+// HEX VALUES
+
+// sets initial random hex value
+document.querySelector('#hex').innerHTML = '#' + (Math.random()* 0xFFFFFF << 0 ).toString(16);
+
+setInterval( changeColor, 1000);
+
+function changeColor(){
+  document.querySelector('#hex').innerHTML = '#' + (Math.random()* 0xFFFFFF << 0 ).toString(16);
+  document.querySelector('#colorChange').style.background = document.querySelector('#hex').innerHTML;
+};
